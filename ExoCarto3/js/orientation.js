9a7fd5949652084195275a7ecc592a2cb17f26af
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-01-06 18:58:45 +0100

    xyz test

--- ExoCarto3/js/orientation.js
+++ ExoCarto3/js/orientation.js
@@ -2,21 +2,41 @@ if (window.DeviceOrientationEvent) {
     window.addEventListener('deviceorientation', function process(event) {
         var alpha = event.alpha;
         var beta = event.beta;
-         var gamma = event.gamma;
+        var gamma = event.gamma;
 	    
         $("#orientation").append(
             $("<p>Orientation de l'appareil :<p> "),
             $("<p>Alpha : "+alpha+"<p>"),
             $("<p>Beta : "+beta+"<p>"),
-            $("<p>Gamma : "+gamma+"<p>")
+            $("<p>Gamma : "+gamma+"<p>"),
+            $("<br><br>")
         );
+
+        setTimeout(stopJump, 3*1000)
     });   
 }
 
-  if (window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', deviceMotionHandler);
-    setTimeout(stopJump, 3*1000);
-  }
+if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function process2(event) {
+        var x = event.accelerationIncludingGravity.x;
+        var y = event.accelerationIncludingGravity.y;
+        var z = event.accelerationIncludingGravity.z;
+	    
+        $("#orientation").append(
+            $("<p>Orientation de l'appareil :<p> "),
+            $("<p>X : "+x+"<p>"),
+            $("<p>Y : "+y+"<p>"),
+            $("<p>Z : "+z+"<p>")
+        );
+
+        setTimeout(stopJump, 3*1000)
+    });   
+}
+
+  
+
+
+
 
 
 
