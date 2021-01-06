6dd2f390142e37397a26869ae3f4320e6eb504ed
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-01-06 11:01:36 +0100

    ajout dossier exo2 + test exo2

---
+++ ExoCarto2/pos.js
@@ -0,0 +1,22 @@
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+    $("#cartContent").append(
+        $("<p>Votre position actuelle est :<p> "),
+        $("<p>Latitude : "+crd.latitude+"<p>"),
+        $("<p>Longitude : "+crd.longitude+"<p>"),
+        $("<p>La précision est de "+crd.accuracy+"mètres.<p>")
+    );
+  }
+  
+  function error(err) {
+    console.warn(`ERREUR (${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
