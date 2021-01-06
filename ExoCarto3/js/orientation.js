18f0784f4f1e5bd93cf71f59fbb3e3dffda060e1
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-01-06 18:32:01 +0100

    alpha beta gamma

--- ExoCarto3/js/orientation.js
+++ ExoCarto3/js/orientation.js
@@ -1,25 +1,22 @@
 if (window.DeviceOrientationEvent) {
-    window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-    document.getElementById("doeSupported").innerText = "Supported!";
-  }
-  else{
-    document.getElementById("doeSupported").innerText = "Not Supported!";
-  }
+    window.addEventListener('deviceorientation', function process(event) {
+        var alpha = event.alpha;
+        var beta = event.beta;
+         var gamma = event.gamma;
+	    
+        $("#orientation").append(
+            $("<p>Orientation de l'appareil :<p> "),
+            $("<p>Alpha : "+alpha+"<p>"),
+            $("<p>Beta : "+beta+"<p>"),
+            $("<p>Gamma : "+gamma+"<p>")
+        );
+    });   
+}
 
   if (window.DeviceMotionEvent) {
     window.addEventListener('devicemotion', deviceMotionHandler);
     setTimeout(stopJump, 3*1000);
   }
 
-function process(event) {
-  var alpha = event.alpha;
-  var beta = event.beta;
-  var gamma = event.gamma;
-  $("#orientation").append(
-        $("<p>Orientation de l'appareil :<p> "),
-        $("<p>Alpha : "+alpha+"<p>"),
-        $("<p>Beta : "+beta+"<p>"),
-        $("<p>Gamma : "+gamma+"mètres.<p>")
-    );
-}
+
 
