1ad4f888e348707dad5b810e9b43d0d4eb14135c
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-01-13 11:57:59 +0100

    js

--- TD2/Exo2/js/script.js
+++ TD2/Exo2/js/script.js
@@ -1,21 +1,37 @@
 
     
+function success(pos) {
+    var crd = pos.coords; 
+
+
+    var layer = new L.StamenTileLayer("watercolor");
+    var map = new L.Map("mapcontainer", {
+        center: new L.LatLng(24.6109106, -76.2487416),
+        zoom: 10
+    });
+    
+    map.addLayer(layer);
+    
+    var polygon = L.polygon([
+        [25.7825453, -80.2994984],
+        [32.3192214, -64.906485],
+        [17.9702607, -66.6639921]
+    ]).addTo(map);  
+    
+    var circle = L.circle([crd.latitude, longitude], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: crd.accuracy
+    }).addTo(mymap);
+  }  
+  function error(err) {
+    console.warn(`ERREUR (${err.code}): ${err.message}`);
+  }
   
+  navigator.geolocation.getCurrentPosition(success, error, options);
 
-var layer = new L.StamenTileLayer("watercolor");
-var map = new L.Map("mapcontainer", {
-    center: new L.LatLng(24.6109106, -76.2487416),
-    zoom: 6
-});
 
-map.addLayer(layer);
-
-var polygon = L.polygon([
-    [25.7825453, -80.2994984],
-    [32.3192214, -64.906485],
-    [17.9702607, -66.6639921]
-]).addTo(map);  
-  
   
 
   
