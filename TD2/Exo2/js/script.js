3417d2c7569f161e0665ccc82f7cf89c9fd0e55f
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-01-13 10:55:45 +0100

    ex2

---
+++ TD2/Exo2/js/script.js
@@ -0,0 +1,14 @@
+
+    
+  
+
+var layer = new L.StamenTileLayer("toner");
+var map = new L.Map("mapcontainer", {
+    center: new L.LatLng(24.6109106, -76.2487416),
+    zoom: 12
+});
+map.addLayer(layer);
+  
+  
+  
+  
