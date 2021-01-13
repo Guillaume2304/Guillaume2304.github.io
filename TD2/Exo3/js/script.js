cd8bb31ddc90349a726d208996a52a1e95add2e9
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-01-13 20:25:36 +0100

    exo3

--- TD2/Exo3/js/script.js
+++ TD2/Exo3/js/script.js
@@ -1,4 +1,4 @@
-const url="https://api-adresse.data.gouv.fr/search/?postcode=06250";
+const url="https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&postcode=44380";
 
 window.addEventListener("DOMContentLoaded", (event) => {
     $.getJSON(url, function(result){
