f7973beb2e34ef682a4e5dd878d00759f57700e6
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-01-13 11:39:40 +0100

    js

--- TD2/Exo2/js/script.js
+++ TD2/Exo2/js/script.js
@@ -2,7 +2,7 @@
     
   
 
-var layer = new L.StamenTileLayer("toner");
+var layer = new L.StamenTileLayer("watercolor");
 var map = new L.Map("mapcontainer", {
     center: new L.LatLng(24.6109106, -76.2487416),
     zoom: 12
