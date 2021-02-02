b41edc93a7f602d7903de81ae0628460890a47b5
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-02-02 10:08:53 +0100

    ex2

--- TD3/Exo2/js/script.js
+++ TD3/Exo2/js/script.js
@@ -2,7 +2,9 @@
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.z=100;
+
+camera.position.set(2,0,5);
+
 let rendu = new THREE.WebGLRenderer();
 rendu.setSize(window.innerWidth, window.innerHeight);
 rendu.setClearColor(0x000000);
@@ -11,11 +13,19 @@ document.body.appendChild(rendu.domElement);
 
 let geometry = new THREE.BoxGeometry();
 
-let material = new THREE.MeshBasicMaterial({color: 0xD2691E});
+let material = new THREE.MeshLambertMaterial({color: 0xD2691E});
+
+const texture = new THREE.TextureLoader().load( '../textures/texture.jpg' );
 
 let cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
+
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.5 );
+scene.add( directionalLight );
 
 camera.position.z=5;
 
@@ -26,4 +36,5 @@ let animer = function animer(){
 
     rendu.render(scene, camera);
 }
+
 animer();
