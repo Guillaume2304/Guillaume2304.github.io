5eff726798344703bb6aac3adeaef0576907d457
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-02-09 20:27:58 +0100

    td5

--- TD5/js/script.js
+++ TD5/js/script.js
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
               ctx.translate(arrow.width/2,arrow.height/2);
               if (angle > 0){ctx.rotate(-angle*Math.PI/180);}
               else {ctx.rotate(angle*Math.PI/180);} 
-              ctx.drawImage(arrow, 0, 0, arrow.width, arrow.width, 0, 0, canvas.width/2, canvas.height/2 );
+              ctx.drawImage(arrow, 0, 0, -arrow.width, -arrow.width, 0, 0, canvas.width/2, canvas.height/2 );
               //ctx.setTransform(1, 0, 0, 1, 0, 0);
               ctx.restore();
               
