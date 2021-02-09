96042507c6c2e76f8159186252df24a37c320f75
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-02-09 10:44:18 +0100

    td5

--- TD5/js/script.js
+++ TD5/js/script.js
@@ -3,20 +3,38 @@ window.addEventListener("DOMContentLoaded", (event) => {
         window.addEventListener('deviceorientation', function process(event) {
             var alpha = event.alpha;
             var beta = event.beta;
-            var gamma = event.gamma;        
+            var gamma = event.gamma;  
+            
+            var options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            };
+
+            function success(pos) {
+                var crd = pos.coords;
+                var northAngle = crd.heading;
+            }
+            
+            function error(err) {
+                console.warn(`ERREUR (${err.code}): ${err.message}`);
+            }
+            
+            navigator.geolocation.getCurrentPosition(success, error, options);
+               
           
             var canvas = document.getElementById('canvas');
             if (canvas.getContext) {
               var ctx = canvas.getContext('2d');
               ctx.drawImage(compass, 0, 0, compass.width, compass.height, 0, 0, canvas.width, canvas.height );
-             
-              ctx.rotate(alpha); 
+              ctx.translate(canvas.width/2,canvas.height/2);
+              ctx.rotate(alpha*Math.PI/180); 
               ctx.drawImage(arrow, 0, 0, arrow.width, arrow.height, 0, 0, canvas.width, canvas.height );
-              ctx.setTransform(1, 0, 0, 1, 0, 0);
+              //ctx.setTransform(1, 0, 0, 1, 0, 0);
+              //ctx.restore();
               
             }
             
         });   
     }
- 
 })
