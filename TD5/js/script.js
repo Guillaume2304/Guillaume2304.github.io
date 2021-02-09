b58eff12ca81f8306920259cd3d3d0924ef7ebb6
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-02-09 10:46:50 +0100

    td5

--- TD5/js/script.js
+++ TD5/js/script.js
@@ -27,11 +27,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
             if (canvas.getContext) {
               var ctx = canvas.getContext('2d');
               ctx.drawImage(compass, 0, 0, compass.width, compass.height, 0, 0, canvas.width, canvas.height );
+              ctx.save();
               ctx.translate(canvas.width/2,canvas.height/2);
               ctx.rotate(alpha*Math.PI/180); 
               ctx.drawImage(arrow, 0, 0, arrow.width, arrow.height, 0, 0, canvas.width, canvas.height );
               //ctx.setTransform(1, 0, 0, 1, 0, 0);
-              //ctx.restore();
+              ctx.restore();
               
             }
             
