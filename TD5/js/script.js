68c0acffa838a14aba3d3bf3f0378cfac3689b5a
Guillaume2304 <guillaume.barault@etu.unice.fr>
2021-02-09 09:52:35 +0100

    td5

---
+++ TD5/js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    if (window.DeviceOrientationEvent) {
+        window.addEventListener('deviceorientation', function process(event) {
+            var alpha = event.alpha;
+            var beta = event.beta;
+            var gamma = event.gamma;        
+          
+            var canvas = document.getElementById('canvas');
+            if (canvas.getContext) {
+              var ctx = canvas.getContext('2d');
+              ctx.drawImage(compass, 0, 0, compass.width, compass.height, 0, 0, canvas.width, canvas.height );
+             
+              ctx.rotate(alpha); 
+              ctx.drawImage(arrow, 0, 0, arrow.width, arrow.height, 0, 0, canvas.width, canvas.height );
+           
+              
+            }
+            
+        });   
+    }
+ 
+})
